9df6807ee8f2d4bffe16040425c8c11717b31090
snehav111 <snehav0307@gmail.com>
2025-08-29 14:52:24 +0530

    3RD c++

---
+++ pass.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float marks;
+    cout<<"ENTER YOUR MARKS : "<<endl;
+    cin>>marks;
+
+    if(marks>=33){
+        cout<<"PASS "<<endl;
+    }else{
+        cout<<"FAIL ";
+    }
+
+    return 0 ;
+
+}
